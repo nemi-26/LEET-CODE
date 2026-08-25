1b2d649634e45fbfaefb2a56a5f63bc1911ebd4c
nemi-26 <nemitvl01_bci29@mepcoeng.ac.in>
2026-08-25 16:42:50 +0530

    Create max product of 3 numbers.c

---
+++ max product of 3 numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <limits.h>
+
+int maximumProduct(int* nums, int numsSize) {
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for (int i = 0; i < numsSize; i++) {
+        int n = nums[i];
+        if (n > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        } else if (n > max2) {
+            max3 = max2;
+            max2 = n;
+        } else if (n > max3) {
+            max3 = n;
+        }
+        if (n < min1) {
+            min2 = min1;
+            min1 = n;
+        } else if (n < min2) {
+            min2 = n;
+        }
+    }
+    long long prod1 = (long long)max1 * max2 * max3;
+    long long prod2 = (long long)min1 * min2 * max1;
+    return (prod1 > prod2) ? prod1 : prod2;
+}
